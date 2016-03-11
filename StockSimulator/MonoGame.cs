10e9e20cc12af59c643005e73e6c664b46de08d2
Philip Pratt <pratstercs@yahoo.co.uk>
2016-03-11 10:24:46 +0000

    Added drawGraph method. Added code to test this. Moved drawLine to Graphing class. Added window properties.

--- StockSimulator/MonoGame.cs
+++ StockSimulator/MonoGame.cs
@@ -17,6 +17,11 @@ namespace StockSimulator
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 800;  // set this value to the desired width of your window
+            graphics.PreferredBackBufferHeight = 600;   // set this value to the desired height of your window
+            this.Window.Title = "StockSimulator";
+
+            graphics.ApplyChanges();
         }
 
         /// <summary>
@@ -40,8 +45,9 @@ namespace StockSimulator
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            t = new Texture2D(GraphicsDevice, 1,1);
-            t.SetData<Color>(new Color[] { Color.Red });
+            t = new Texture2D(GraphicsDevice, 1, 1);
+            //t.SetData<Color>(new Color[] { Color.Red });
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -79,32 +85,66 @@ namespace StockSimulator
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            DrawLine(t, new Vector2(200, 200), new Vector2(100, 50));
+
+            decimal[] data = { 53.91M, 57.57M, 58.6M, 59.25M, 58.86M, 58.17M, 58.88M, 58.09M, 56.21M, 57.03M, 58.11M, 57.605M, 59.2M, 59.9M, 59.96M, 60.55M, 60.05M, 59.5M, 59.54M, 59.67M };
+            Vector2[] points = Utilities.pointMaker(data, 600, 800, 0, 0);
+            Graphing.drawGraph(t, spriteBatch, Color.Lime, points);
             spriteBatch.End();
 
             base.Draw(gameTime);
         }
+    }
 
-        void DrawLine(Texture2D texture,Vector2 start, Vector2 end)
+    public static class Graphing
+    {
+        /// <summary>
+        /// Method to draw a line between two points
+        /// </summary>
+        /// <param name="t">The texture object to use</param>
+        /// <param name="sb">The SpriteBatch</param>
+        /// <param name="color">The line colour</param>
+        /// <param name="start">The co-ordinates of the starting point</param>
+        /// <param name="end">The co-ordinates of the ending point</param>
+        public static void drawLine(Texture2D t, SpriteBatch sb, Color color, Vector2 start, Vector2 end)
         {
+            t.SetData<Color>(new Color[] { color });
+
             Vector2 edge = end - start;
             // calculate angle to rotate line
             float angle =
                 (float)System.Math.Atan2(edge.Y, edge.X);
 
 
-            spriteBatch.Draw(t,
+            sb.Draw(t,
                 new Rectangle(// rectangle defines shape of line and position of start of line
                     (int)start.X,
                     (int)start.Y,
                     (int)edge.Length(), //sb will strech the texture to fill this rectangle
-                    1), //width of line, change this to make thicker line
+                    3), //width of line, change this to make thicker line
                 null,
-                Color.Red, //colour of line
+                color,
                 angle,     //angle of line (calulated above)
                 new Vector2(0, 0), // point in line about which to rotate
                 SpriteEffects.None,
                 0);
+        }
+
+        /// <summary>
+        /// Method to draw lines between a specified set of points, i.e. a graph
+        /// </summary>
+        /// <param name="t">The Texture to use</param>
+        /// <param name="sb">The SpriteBatch</param>
+        /// <param name="color">The Colour for the lines</param>
+        /// <param name="data">The co-ordinates of the points to graph</param>
+        public static void drawGraph(Texture2D t, SpriteBatch sb, Color color, Vector2[] data)
+        {
+            Vector2 prevPoint = new Vector2(data[0].X, data[0].Y);
+
+            foreach (Vector2 point in data)
+            {
+                drawLine(t, sb, color, prevPoint, point);
+                prevPoint = point;
+            }
         }
     }
 }
