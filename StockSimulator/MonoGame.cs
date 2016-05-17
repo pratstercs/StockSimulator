cde8b0f4aa1e0fc19b6973dd15aefb9caca3d241
Philip Pratt <pratstercs@yahoo.co.uk>
2016-05-17 17:04:12 +0100

    No idea, to be deleted later

--- StockSimulator/MonoGame.cs
+++ StockSimulator/MonoGame.cs
@@ -180,48 +180,50 @@ namespace StockSimulator
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            //GraphicsDevice.Clear(Color.LightGray);
-            //spriteBatch.Begin();
+            GraphicsDevice.Clear(Color.LightGray);
+            spriteBatch.Begin();
 
-            //Graphing.drawLine(t, spriteBatch, Color.Black, new Vector2(0, WINDOW_HEIGHT * 0.08f), new Vector2(WINDOW_WIDTH, WINDOW_HEIGHT * 0.08f), 35);
-            //Graphing.drawLine(t, spriteBatch, Color.Black, new Vector2(0, WINDOW_HEIGHT * 0.875f), new Vector2(WINDOW_WIDTH, WINDOW_HEIGHT * 0.875f), 5);
+            Texture2D t = new Texture2D(GraphicsDevice, 1, 1);
 
-            //gl.getAllChanges();
-            //float offset = 0;
-            //foreach (StockChange sc in gl.changes.Values)
-            //{
-            //    offset += 1.5f * DrawTickerString(spriteBatch, f_120, new string[] { sc.symbol, sc.change.ToString() }, new Vector2(width - (tickerScroll + offset), height * 0.085f));
-            //}
+            Graphing.drawLine(t, spriteBatch, Color.Black, new Vector2(0, WINDOW_HEIGHT * 0.08f), new Vector2(WINDOW_WIDTH, WINDOW_HEIGHT * 0.08f), 35);
+            Graphing.drawLine(t, spriteBatch, Color.Black, new Vector2(0, WINDOW_HEIGHT * 0.875f), new Vector2(WINDOW_WIDTH, WINDOW_HEIGHT * 0.875f), 5);
 
-            //System.DateTime start = new System.DateTime(2015, 1, 1);
-            //System.DateTime end = new System.DateTime(2016, 1, 1);
-            //System.DateTime[] dates = gl.getStockDates("JPM", start, end);
+            gl.getAllChanges();
+            float offset = 0;
+            foreach (StockChange sc in gl.changes.Values)
+            {
+                offset += 1.5f * DrawTickerString(spriteBatch, f_120, new string[] { sc.symbol, sc.change.ToString() }, new Vector2(WINDOW_WIDTH - (tickerScroll + offset), WINDOW_HEIGHT * 0.085f));
+            }
 
-            //decimal[][] data = gl.getStockDataByDay("JPM", start, end);
-            //Color[] colours = { Color.PeachPuff, Color.Navy, Color.Green, Color.MonoGameOrange };
+            System.DateTime start = new System.DateTime(2015, 1, 1);
+            System.DateTime end = new System.DateTime(2016, 1, 1);
+            System.DateTime[] dates = gl.getStockDates("JPM", start, end);
 
-            //float[] values = Graphing.initialiseGraph(spriteBatch, t, f_120, data, dates, 780, 1620, 150, 150); //draw basics of graph and calculate actual plot area excluding margins, etc
+            decimal[][] data = gl.getStockDataByDay("JPM", start, end);
+            Color[] colours = { Color.PeachPuff, Color.Navy, Color.Green, Color.MonoGameOrange };
 
-            //for (int i = 0; i < 4; i++)
-            //{
-            //    Vector2[] points = Graphing.pointMaker(data[i], values[0], values[1], values[2], values[3], values[4], values[5]);
-            //    Graphing.drawGraph(GraphicsDevice, spriteBatch, colours[i], points);
-            //}
+            float[] values = Graphing.initialiseGraph(spriteBatch, t, f_120, data, dates, 780, 1620, 150, 150); //draw basics of graph and calculate actual plot area excluding margins, etc
 
-            //spriteBatch.End();
+            for (int i = 0; i < 4; i++)
+            {
+                Vector2[] points = Graphing.pointMaker(data[i], values[0], values[1], values[2], values[3], values[4], values[5]);
+                Graphing.drawGraph(GraphicsDevice, spriteBatch, colours[i], points);
+            }
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
 
-            switch(state)
-            {
-                case GameState.MainMenu:
-                    drawMainMenu();
-                    break;
-                case GameState.Sandbox:
-                    break;
-                default:
-                    break;
-            }
+            //switch(state)
+            //{
+            //    case GameState.MainMenu:
+            //        drawMainMenu();
+            //        break;
+            //    case GameState.Sandbox:
+            //        break;
+            //    default:
+            //        break;
+            //}
         }
 
         private void drawMainMenu()
